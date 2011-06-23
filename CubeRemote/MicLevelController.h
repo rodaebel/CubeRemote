be14cfd73aa883b3c571745f186f05db077df6c7
rodaebel <tobias.rodaebel@googlemail.com>
2011-06-24 01:56:23 +0200

    Started implementing mic level controller.

---
+++ CubeRemote/MicLevelController.h
@@ -0,0 +1,16 @@
+//
+//  MicLevelController.h
+//  CubeRemote
+//
+//  Created by Tobias on 6/24/11.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface MicLevelController : NSObject {
+@private
+    
+}
+
+@end
---
+++ CubeRemote/MicLevelController.m
@@ -0,0 +1,28 @@
+//
+//  MicLevelController.m
+//  CubeRemote
+//
+//  Created by Tobias on 6/24/11.
+//
+
+#import "MicLevelController.h"
+
+
+@implementation MicLevelController
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+@end
